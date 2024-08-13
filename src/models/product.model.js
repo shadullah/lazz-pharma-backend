9cc3a65645fbf0264b7dc632607388cc6fb2bbe9
hexaalif <shadullahsad9@gmail.com>
2024-08-13 20:20:10 +0600

    access , refresh token, jwt, aggregate pipeline added

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const productSchema = new mongoose.Schema(
   {
@@ -38,5 +39,7 @@ const productSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+productSchema.plugin(mongooseAggregatePaginate);
 
 export const Product = mongoose.model("Product", productSchema);
